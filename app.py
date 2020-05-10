b9d0d1610da8da0639a053a299bd0fd5dd74723e
Neel Ray <lolneelsd@gmail.com>
2020-05-10 03:37:24 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask,render_template
-
+from flask_pymongo import PyMongo
 import pandas as pd
 import json
 
-
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/covid"
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    #df = pd.read_csv('/Users/sunidhibrajesh/PycharmProjects/TwitterAnalytics/Files/Global_Mobility_Report.csv')
-    ob = {'Date':['2009-03-31','2009-03-30'],'High':[18.79,17.76]}
-    df = pd.DataFrame(data=ob)
-    chart_data = df.to_dict(orient='records')
-    chart_data = json.dumps(chart_data, indent=2)
-    data = {'chart_data': chart_data}
-    return render_template("index.html", data=data)
+    # ob = {'Date':['2009-03-31','2009-03-30'],'High':[18.79,17.76]}
+    # df = pd.DataFrame(data=ob)
+    # chart_data = df.to_dict(orient='records')
+    # chart_data = json.dumps(chart_data, indent=2)
+    # data = {'chart_data': chart_data}
+    covid = mongo.db.state.find_one()
+    print(covid)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
